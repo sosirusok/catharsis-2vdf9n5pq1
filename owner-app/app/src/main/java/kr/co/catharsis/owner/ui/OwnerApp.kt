b543a4d80f672b46e145ce9b35c89797cf0f332a
sosirusok <sosirusok@naver.com>
2026-08-15 14:42:36 +0900

    Fix owner app Compose compilation

--- owner-app/app/src/main/java/kr/co/catharsis/owner/ui/OwnerApp.kt
+++ owner-app/app/src/main/java/kr/co/catharsis/owner/ui/OwnerApp.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
